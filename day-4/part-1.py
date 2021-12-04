993ab54e4723d9a7ef61f0638a01f8d8e9c7c755
Moishe Lettvin <moishel@gmail.com>
2021-12-04 09:53:02 -0700

    Day 4 part 1

---
+++ day-4/part-1.py
@@ -0,0 +1,56 @@
+import functools
+
+def print_board(number, board):
+  sum = 0
+  for row in board:
+    els = [x[0] for x in filter(lambda x: not x[1], row)]
+    if els:
+      sum += functools.reduce(lambda x, y: x + y, els)
+    print(" ". join("%s: %s" % (str(x[0]).rjust(3, ' '), 'X' if x[1] else '0') for x in row))
+
+  print("Solution: %d" % (sum * number))
+
+f = open('/Users/moishe/src/aoc-21/day-4/input/input.txt')
+
+# first line is the random numbers
+
+numbers = [int(x) for x in f.readline().rstrip().split(',')]
+
+boards = []
+current_board = []
+for l in f:
+  if not len(l.rstrip()):
+    if current_board:
+      boards.append(current_board)
+    current_board = []
+    continue
+  current_board.append([(int(x), False) for x in filter(None, l.rstrip().split(' '))])
+
+boards.append(current_board)
+
+for number in numbers:
+  print("Evaluating: %d" % number)
+  for board in boards:
+    for row in board:
+      for i in range(0, len(row)):
+        if row[i][0] == number:
+          row[i] = (number, True)
+
+  # check for winners
+  last_winning_board = None
+  for board in boards:
+    # first by row
+    for row in board:
+      all_true = functools.reduce(lambda x, y: x and y, [x[1] for x in row])
+      if all_true:
+        print("Winning board:")
+        print_board(number, board)
+        exit(0)
+
+    # then by column
+    for i in range(0, len(board[0])):
+      all_true = functools.reduce(lambda x, y: x and y, [x[i][1] for x in board])
+      if all_true:
+        print("Winning board:")
+        print_board(number, board)
+        exit(0)
