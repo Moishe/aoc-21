0ae90674120216cc64b096358bcc99b0a266ecf7
Moishe Lettvin <moishel@gmail.com>
2021-12-09 11:03:22 -0800

    Day 8

--- day-8/part-1.py
+++ day-8/part-1.py
@@ -1,7 +1,7 @@
 import json
 from collections import defaultdict
 
-f = open('/Users/moishe/src/aoc-21/day-8/input/debug.txt')
+f = open('/Users/moishe/src/aoc-21/day-8/input/input.txt')
 
 def get_substrings(s):
   return list(filter(None, s.split(' ')))
@@ -12,40 +12,11 @@ def set_from_item(items, idx):
 def list_from_item(items, idx):
   return list(items)[idx]
 
-def decode_digit(s):
-  m = {
-    'd': 'a',
-    'e': 'b',
-    'a': 'c',
-    'f': 'd',
-    'g': 'e',
-    'b': 'f',
-    'c': 'g'
-  }
-
-
 instances = defaultdict(list)
 
-known_segment_list = [
-  ['a', 'b', 'c',      'e', 'f', 'g'], # 0, 6
-  [          'c',      'f'          ], # 1, 2
-  ['a',      'c', 'd', 'e',      'g'], # 2, 5
-  ['a',      'c', 'd',      'f', 'g'], # 3, 5
-  [     'b', 'c', 'd',      'f'     ], # 4, 4
-  ['a', 'b',      'd',      'f', 'g'], # 5, 5
-  ['a', 'b',      'd', 'e', 'f', 'g'], # 6, 6
-  ['a',      'c',           'f'     ], # 7, 3
-  ['a', 'b', 'c', 'd', 'e', 'f', 'g'], # 8, 7
-  ['a', 'b', 'c', 'd',      'f', 'g'], # 9, 6
-]
-
-known_segment_map = {}
-for s in known_segment_list:
-  known_segment_map[len(s)] = s
-
-segment_lists_by_length = defaultdict(set)
-
+total_sum = 0
 for l in f:
+  segment_lists_by_length = defaultdict(set)
   (input, output) = l.rstrip().split("|")
 
   input_digits = get_substrings(input)
@@ -53,7 +24,7 @@ for l in f:
 
   for s in input_digits + output_digits:
     segment_lists_by_length[len(s)].add(''.join(sorted(s)))
-  print(segment_lists_by_length)
+  #print(segment_lists_by_length)
 
   # deduction rules:
 
@@ -69,16 +40,54 @@ for l in f:
   segment_map = {}
   number_map = {}
 
-  number_map[1] = list_from_item(segment_lists_by_length[4], 0)
-  number_map[4] = list_from_item(segment_lists_by_length[4], 0)
-  number_map[7] = list_from_item(segment_lists_by_length[3], 0)
-  number_map[8] = list_from_item(segment_lists_by_length[7], 0)
+  number_map[1] = set_from_item(segment_lists_by_length[2], 0)
+  number_map[4] = set_from_item(segment_lists_by_length[4], 0)
+  number_map[7] = set_from_item(segment_lists_by_length[3], 0)
+  number_map[8] = set_from_item(segment_lists_by_length[7], 0)
 
   segment_map['a'] = list(set_from_item(segment_lists_by_length[3], 0) - set_from_item(segment_lists_by_length[2], 0))
   
-  maybe_six_or_zero = [set([c for c in x]) for x in list(segment_lists_by_length[6])]
-  for i in maybe_six_or_zero:
-    print(''.join(sorted(list(i))))
-  print(json.dumps(segment_map))
-  print(json.dumps(number_map))
-  exit(0)
+  maybe_six_or_zero_or_nine = [set([c for c in x]) for x in list(segment_lists_by_length[6])]
+  #print(''.join(sorted(list(number_map[1]))))
+  maybe_zero_or_nine = []
+  for i in maybe_six_or_zero_or_nine:
+    if len(list(number_map[1].intersection(i))) == 1:
+      number_map[6] = i
+    else:
+      maybe_zero_or_nine.append(i)
+
+  maybe_five = [set([c for c in x]) for x in list(segment_lists_by_length[5])]
+  for i in maybe_five:
+    if len(list(number_map[6].intersection(i).intersection(number_map[1]))) == 0:
+      number_map[2] = i
+    elif len(list(number_map[1].intersection(i))) == 2:
+      number_map[3] = i
+    else:
+      number_map[5] = i
+
+  middle_line = number_map[2].intersection(number_map[4])
+  middle_line = number_map[5].intersection(middle_line)
+  #print("middle line ", list(middle_line))
+  for i in maybe_zero_or_nine:
+    if len(list(middle_line.intersection(i))) == 0:
+      number_map[0] = i
+    else:
+      number_map[9] = i
+
+  map_to_number = {}
+  #print(json.dumps(segment_map))
+  for k in sorted(number_map.keys()):
+    #print("%s: %s" % (str(k), list(number_map[k])))
+    map_to_number[''.join(sorted(list(number_map[k])))] = k
+
+  #print(json.dumps(map_to_number, indent = 2))
+
+  total_digit = ''
+  for digit in output_digits:
+    sorted_digit = ''.join(sorted([c for c in digit]))
+    total_digit += str(map_to_number[sorted_digit])
+
+  print("%s: %d" % (total_digit, int(total_digit)))
+  total_sum += int(total_digit)
+
+print("sum: %d" % total_sum)
