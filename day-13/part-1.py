26aa0f3a3a580ab9099ebf8d9c0d71e03c16040b
Moishe Lettvin <moishel@gmail.com>
2021-12-13 17:45:18 -0700

    Day 13, part 1 & 2

---
+++ day-13/part-1.py
@@ -0,0 +1,58 @@
+from collections import defaultdict
+
+f = open('/Users/moishe/src/aoc-21/day-13/input/input.txt')
+
+locations = defaultdict(int)
+folds = []
+width = 0
+height = 0
+
+def print_locations():
+  global locations
+  for y in range(0, height + 1):
+    row = ''
+    for x in range(0, width + 1):
+      if (x,y) in locations:
+        row += 'X'
+      else:
+        row += ' '
+    print(row)
+
+for l in f:
+  if l.startswith('fold along'):
+    (axis, coordinate) = l.rstrip().split(' ')[2].split('=')
+    folds.append((axis,int(coordinate)))
+  elif ',' in l:
+    (x,y) = [int(x) for x in l.rstrip().split(',')]
+    width = max(x, width)
+    height = max(y, height)
+    locations[(x,y)] += 1
+
+print_locations()
+print()
+
+for (idx, fold) in enumerate(folds):
+  (axis, coordinate) = fold
+  if axis == 'y':
+    new_locations = defaultdict(int)
+    for location in locations:
+      (x,y) = location
+      if y >= coordinate:
+        y = coordinate - (y - coordinate)
+      new_locations[(x,y)] += 1
+    locations = new_locations
+    height = coordinate - 1
+  elif axis == 'x':
+    new_locations = defaultdict(int)
+    for location in locations:
+      (x,y) = location
+      if x >= coordinate:
+        x = coordinate - (x - coordinate)
+      new_locations[(x,y)] += 1
+    locations = new_locations
+    width = coordinate - 1
+
+  if idx == 1:
+    print(len(locations.keys()))
+
+print_locations()
