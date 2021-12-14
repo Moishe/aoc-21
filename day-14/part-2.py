1e13247112d56e6f7d91f878f918fbeb3430afc9
Moishe Lettvin <moishel@gmail.com>
2021-12-14 16:34:35 -0700

    Part 2 with debug code

--- day-14/part-2.py
+++ day-14/part-2.py
@@ -16,7 +16,7 @@ for i in range(0, len(start) - 1):
   pairs[start[i:i+2]] += 1
 
 state = start
-for i in range(0, 3):
+for i in range(0, 10):
   new_state = ''
   # method 1
   for j in range(0, len(state) - 1):
@@ -29,20 +29,23 @@ for i in range(0, 3):
   
   # method 2
   new_pairs = pairs.copy()
-  for pair in pairs:
-    new_pairs[pair[0] + xlat[pair]] += 1
-    new_pairs[xlat[pair] + pair[1]] += 1
-    new_pairs[pair] -= 1
+  for (pair, count) in pairs.items():
+    new_pairs[pair[0] + xlat[pair]] += count
+    new_pairs[xlat[pair] + pair[1]] += count
+    new_pairs[pair] -= count
+    if (new_pairs[pair] == 0):
+      del(new_pairs[pair])
   pairs = new_pairs
 
   # check
   expected_pairs = defaultdict(int)
-  for i in range(0, len(start) - 1):
-    expected_pairs[start[i:i+2]] += 1
+  for i in range(0, len(state) - 1):
+    expected_pairs[state[i:i+2]] += 1
 
-  print(json.dumps(pairs, indent=2))
-  print(json.dumps(expected_pairs, indent=2))
-
+  for pair in expected_pairs:
+    if expected_pairs[pair] != pairs[pair]:
+      print("Mismatch %s %d %d" % (pair, expected_pairs[pair], pair[pair]))
+      exit(1)
 
 freqs = defaultdict(int)
 for c in state:
