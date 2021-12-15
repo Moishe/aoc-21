c25cb7b3d289c5d41fc5a02049a7795fa2fa5655
Moishe Lettvin <moishel@gmail.com>
2021-12-15 11:19:09 -0700

    Day 15, parts 1 and 2, using Dijkstra

--- day-15/part-1.py
+++ day-15/part-1.py
@@ -1,17 +1,16 @@
 import heapq
+import sys
 
 f = open('/Users/moishe/src/aoc-21/day-15/input/input.txt')
+
+highlight = '\033[1m'
+endc = '\033[0m'
 
 width = 0
 height = 0
 board = []
 queue = []
 neighbors = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-for l in f:
-  width = len(l.rstrip())
-  height += 1
-  board.extend([int(c) for c in l.rstrip()])
 
 def idx_to_x_y(idx):
   global width
@@ -21,33 +20,93 @@ def x_y_to_idx(xy): #xy is a tuple
   global width
   return xy[1] * width + xy[0]
 
-def enqueue(xy, prev_cost, path):
-  global queue
-  if xy not in path:
-    cost = prev_cost + float(board[x_y_to_idx(xy)]) / float(len(path) + 1)
-    cost -= width - xy[0] + height - xy[1]
-    path.append(xy)
-    heapq.heappush(queue, (cost, xy, path))
+def print_board_with_highlight(path):
+  for y in range(0, height):
+    row = ''
+    for x in range(0, width):
+      val = str(board[x_y_to_idx((x,y))])
+      if (x,y) in path:
+        row += highlight + val + endc
+      else:
+        row += val
+    print(row)
 
-# breadth first search the board
-enqueue((0,0), 0, [])
+for l in f:
+  width = len(l.rstrip())
+  height += 1
+  board.extend([int(c) for c in l.rstrip()])
 
-found_path = False
-iterations = 0
-while not found_path and len(queue):
+part2 = True
+debug_print = False
+if part2:
+  if debug_print:
+    for y in range(0, height):
+      row = ''
+      for x in range(0, width):
+        row += str(board[x_y_to_idx((x,y))])
+      print(row)
+
+  new_board = [0] * len(board) * 25
+
+  for x in range(0, 5):
+    for y in range(0, 5):
+      for (idx, cost) in enumerate(board):
+        (bx,by) = idx_to_x_y(idx)
+        newx = bx + x * width
+        newy = by + y * height
+        newidx = newx + newy * width * 5
+
+        new_cost = (cost + x + y)
+        new_cost = new_cost if new_cost <= 9 else new_cost % 10 + 1
+        new_board[newidx] = new_cost
+
+  width = width * 5
+  height = height * 5
+  board = new_board
+
+# lowest cost to get to each node
+board_costs = [sys.maxsize] * len(board)
+board_paths = [[]] * len(board)
+
+unvisited_nodes = set()
+for i in range(0, len(board)):
+  unvisited_nodes.add(idx_to_x_y(i))
+
+found = False
+
+# start at 0,0
+board_costs[0] = 0
+queue = []
+heapq.heappush(queue, (0, (0,0), []))
+
+while not found:
   (cost, xy, path) = heapq.heappop(queue)
-  iterations += 1
+  unvisited_nodes.remove(xy)
   if (xy == (width - 1, height - 1)):
-    found_path = True
-  for neighbor in neighbors:
-    (x1, y1) = (xy[0] + neighbor[0], xy[1] + neighbor[1])
-    if x1 >= 0 and x1 < width and y1 >= 0 and y1 < height:
-      enqueue((x1,y1), cost, path.copy())
-
-if found_path:
-  total_cost = -board[0]
-  for xy in path:
-    total_cost += board[x_y_to_idx(xy)]  
-  print("Found it. Total cost is %d\n" % total_cost, iterations)
-else:
-  print(":shrug:")
+    print("Found it")
+    print(highlight + str(cost) + endc)
+    if debug_print:
+      print_board_with_highlight(path)
+    found = True
+  else:
+    changed = []
+    for neighbor in neighbors:
+      (x1, y1) = (xy[0] + neighbor[0], xy[1] + neighbor[1])
+      if x1 >= 0 and x1 < width and y1 >= 0 and y1 < height:
+        if (x1,y1) in unvisited_nodes:
+          idx = x_y_to_idx((x1,y1))
+          tentative_cost = cost + board[idx]
+          if tentative_cost < board_costs[idx]:
+            was_infinite = board_costs[idx] == sys.maxsize
+            board_costs[idx] = tentative_cost
+            board_paths[idx] = path + [xy]
+            if (was_infinite):
+              heapq.heappush(queue, (board_costs[idx], (x1, y1), board_paths[idx]))
+            else:
+              changed.append((x1,y1))
+    if changed:
+      for (idx, (cost, xy, path)) in enumerate(queue):
+        if xy in changed:
+          board_idx = x_y_to_idx(xy)
+          queue[idx] = (board_costs[board_idx], xy, board_paths[board_idx])
+      heapq.heapify(queue)
